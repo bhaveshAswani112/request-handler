59c094fb1b68ab9bffe03a4ebe4960930f1519fe
bhaveshAswani112 <aswanib133@gmail.com>
2025-01-18 13:14:44 +0530

    pm2 configured

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: "upload",
-        script: "./dist/index.js",
-        env: {
-          PORT: 5000,
-          NODE_ENV: "production",
-        },
-      }
-    ],
-  };
-  
+  apps: [
+    {
+      name: "request-handler",
+      script: "./dist/index.js",
+      instances: "max", // Use all available CPU cores
+      exec_mode: "cluster", // Enable cluster mode for load balancing
+      env: {
+        PORT: 5000,
+        NODE_ENV: "production",
+      },
+      
+    },
+  ],
+};
