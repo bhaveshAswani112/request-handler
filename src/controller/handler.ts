9989fae93883e6aea58334cbe1079f1633754b92
bhaveshAswani112 <aswanib133@gmail.com>
2025-01-18 12:26:30 +0530

    some small bug fixes

--- src/controller/handler.ts
+++ src/controller/handler.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express"
 import { downloadSourceCode } from "../utils/helper"
 import * as AWS from "aws-sdk"
 import { ACCESS_KEY_ID,SECRET_ACCESS_KEY,S3_URL,dirname } from "../constant.js";
-import * as path from "path"
-import fs from "fs"
 
 
 const s3Client = new AWS.S3({
@@ -17,14 +15,21 @@ export const requestHandler = async (req : Request, res : Response) : Promise<an
         try {
             const host = req.hostname
             const id = host.split(".")[0]
-            let filePath = req.path ? req.path : "index.html"
+            
+            let filePath = req.path==="/" ? "index.html" : req.path 
             console.log(filePath)
             filePath = filePath.startsWith("/") ? filePath.substring(1) : filePath
             const contents = await s3Client.getObject({
                 Bucket : "react-bucket",
                 Key : `output/${id}/dist/${filePath}`
             }).promise()
-            const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+            const type = filePath.endsWith("html") ? "text/html" :
+             filePath.endsWith("css") ? "text/css" :
+             filePath.endsWith("js") ? "application/javascript" :
+             filePath.endsWith("svg") ? "image/svg+xml" :
+             filePath.endsWith("jpg") || filePath.endsWith("jpeg") ? "image/jpeg" :
+             filePath.endsWith("png") ? "image/png" :
+             "application/octet-stream"; // Default for unknown types
             res.set("Content-Type", type);
             res.send(contents.Body)
         } catch (error) {
